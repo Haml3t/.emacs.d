d82699a797d28488dcdedd90ab4560d460a5f1ea
U-YGGDRASIL\Haml3t <lhaml3t@gmail.com>
2015-10-27 02:43:58 -0400

    changes successfully merged

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - YGGDRASIL at <2015-10-27 Tue 02:39>
-(setq stored-clock-history '(("/home/Haml3t/org/TODO.org" . 3043) ("/home/Haml3t/org/ME57200.org_archive" . 353) ("/home/Haml3t/org/refile_YGGDRASIL.org_archive" . 86) ("/home/Haml3t/org/TODO.org" . 4250) ("/home/Haml3t/org/TODO.org" . 2084)))
+;; org-persist.el - YGGDRASIL at <2015-10-27 Tue 02:43>
+(setq stored-clock-history '(("/home/Haml3t/org/TODO.org" . 3043) ("/home/Haml3t/org/ME57200.org_archive" . 353) ("/home/Haml3t/org/refile_YGGDRASIL.org_archive" . 86) ("/home/Haml3t/org/TODO.org" . 4251) ("/home/Haml3t/org/TODO.org" . 2084) ("/cygdrive/c/cygwin64/home/Haml3t/org/refile_YGGDRASIL.org" . 20) ("/cygdrive/c/cygwin64/home/Haml3t/org/refile_YGGDRASIL.org" . 20)))
