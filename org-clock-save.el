48d3fa3f50777ead1fa6199b09cdb72fc1255c53
U-YGGDRASIL\Haml3t <lhaml3t@gmail.com>
2015-10-27 02:39:52 -0400

    some org stuff

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,1 +1,2 @@
-;; org-persist.el - YGGDRASIL at <2015-07-13 Mon 18:32>
+;; org-persist.el - YGGDRASIL at <2015-10-27 Tue 02:39>
+(setq stored-clock-history '(("/home/Haml3t/org/TODO.org" . 3043) ("/home/Haml3t/org/ME57200.org_archive" . 353) ("/home/Haml3t/org/refile_YGGDRASIL.org_archive" . 86) ("/home/Haml3t/org/TODO.org" . 4250) ("/home/Haml3t/org/TODO.org" . 2084)))
