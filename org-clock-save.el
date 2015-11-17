5604580f811b7df6db26097b4b97eef98805071c
David Sugarman <lhaml3t@gmail.com>
2015-11-16 19:27:35 -0500

    lua stuff

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,1 +1,2 @@
-;; org-persist.el - DUBSTEP at <2015-11-08 Sun 18:09>
+;; org-persist.el - DUBSTEP at <2015-11-13 Fri 21:00>
+(setq stored-clock-history '(("/home/haml3t/org/refile_DUBSTEP.org" . 1) ("/home/haml3t/org/refile_DUBSTEP.org" . 275)))
