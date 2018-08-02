5e4177edb32b925264a735619f1d8e860afbea4b
David Sugarman <d.m.sugarman@gmail.com>
2018-08-02 16:36:48 -0400

    autocomplete works now whee

---
+++ elpa/lush-theme-20141107.1606/lush-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lush-theme" "20141107.1606" "A dark theme with strong colors" '((emacs "24")) :commit "3b80004f33cdce9f4db69e9ccf2041561e98985d" :keywords '("theme" "dark" "strong colors") :authors '(("Andre Richter" . "andre.o.richter@gmail.com")) :maintainer '("Andre Richter" . "andre.o.richter@gmail.com") :url "https://github.com/andre-richter/emacs-lush-theme")
---
+++ elpa/rsense-20100511.405/rsense-autoloads.el
@@ -0,0 +1,15 @@
+;;; rsense-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("rsense.el") (23395 24874 354519 314000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; rsense-autoloads.el ends here
